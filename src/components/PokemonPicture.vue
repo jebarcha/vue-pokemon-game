7d47b04b4bac8f59ed80ae6e99181ca7c8732d40
jebarcha <jebarcha@hotmail.com>
2021-09-26 17:56:50 -0500

    First Commit

---
+++ src/components/PokemonPicture.vue
@@ -0,0 +1,69 @@
+<template>
+<div class="pokemon-container">
+
+  <img v-if="showPokemon" 
+       :src="imgSrc" 
+       class="fade-in"
+       alt="pokemon">
+
+  <img v-else 
+       :src="imgSrc" 
+       class="hidden-pokemon"
+       alt="pokemon">
+
+  <!-- <img v-if="!showPokemon" 
+       :src="imgSrc" 
+       class="hidden-pokemon"
+       alt="pokemon">
+  <img v-if="showPokemon" 
+       :src="imgSrc" 
+       class="fade-in"
+       alt="pokemon"> -->
+
+</div>
+</template>
+
+<script>
+// Number,required
+
+export default {
+    props: {
+        pokemonId: {
+            type: Number,
+            required: true
+        },
+        showPokemon: {
+            type: Boolean,
+            required: true,
+            default: false
+        }
+    },
+    computed: {
+        imgSrc() {
+            return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${this.pokemonId}.svg`
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+.pokemon-container {
+    display: flex;
+    justify-content: center;
+    height: 200px;
+}
+img {
+    height: 200px;
+    /* position: absolute;
+    right: 38%; */
+    user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    -webkit-user-drag: none;
+    -webkit-user-select: none;
+}
+.hidden-pokemon {
+    filter: brightness(0);
+}
+</style>
---
+++ src/pages/PokemonPage.vue
@@ -0,0 +1,76 @@
+<template>
+  <h1 v-if="!pokemon">Please wait...</h1>
+
+  <div v-else>
+    <h1>Who is this pokemon?</h1>
+    
+    <PokemonPicture :pokemon-id="pokemon.id" 
+                     :show-pokemon="showPokemon" />
+
+    <PokemonOptions :pokemons="pokemonArr"
+                     @selection-pokemon="checkAnswer" />
+  
+    <template v-if="showAnswer" class="fade-in">
+      <h2>{{ message }}</h2>
+      <button @click="newGame()">New Game</button>
+    </template>
+  </div>
+
+</template>
+
+<script>
+import PokemonOptions from '@/components/PokemonOptions.vue'
+import PokemonPicture from '@/components/PokemonPicture.vue'
+
+//import getPokemonOptions from '@/helpers/getPokemonOptions'
+import getPokemonOptions from '../helpers/PokemonOptions'
+
+//console.log(getPokemonOptions());
+
+export default {
+  components: { PokemonPicture, PokemonOptions },
+  data() {
+    return {
+      pokemonArr: [],
+      pokemon: null,
+      showPokemon: false,
+      showAnswer: false,
+      message: ''
+    }
+  },
+  methods: {
+    async mixPokemonArray() {
+      this.pokemonArr = await getPokemonOptions()
+
+      const rndInt = Math.floor(Math.random() * 4)
+      this.pokemon = this.pokemonArr[rndInt]
+    },
+    checkAnswer(selectedId) {
+      this.showPokemon = true
+      this.showAnswer = true
+
+      if (selectedId === this.pokemon.id) {
+        this.message = `Correct, ${this.pokemon.name}`
+      } else {
+        this.message= `Ops, It was ${this.pokemon.name}`
+      }
+    },
+    newGame() {
+      this.showPokemon = false
+      this.showAnswer = false
+      this.pokemonArr = []
+      this.pokemon = null
+      this.mixPokemonArray()
+    }
+  },
+  mounted() {
+    this.mixPokemonArray()
+  }
+}
+</script>
+
+<style>
+  button {
+    cursor: pointer;
+  }
+</style>
